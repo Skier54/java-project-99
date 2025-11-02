4b67083e71c077cf53cd42c98717537a71518eae
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-11-02 13:47:16 +0700

    new file User.java

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN gradle installDist
 
 ENV JAVA_OPTS="-XX:+UseContainerSupport -XX:MaxRAMPercentage=60.0 -XX:InitialRAMPercentage=50.0"
 
-#EXPOSE 8080
+EXPOSE 8080
 
 CMD ./build/install/app/bin/app
---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,96 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements BaseEntity, UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @NotBlank(message = "Фамилия не может быть пустой")
+    @Size(min = 2, max = 30)
+    private String lastName;
+
+    @Column(unique = true)
+    @NotBlank(message = "Почта не может быть пустой")
+    @Email
+    private String email;
+
+    @NotBlank(message = "Пароль не может быть пустой")
+    @Size(min = 2, max = 30)
+    private String passwordDigest;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    @Override
+    public String getPassword() {
+        return passwordDigest;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
+
