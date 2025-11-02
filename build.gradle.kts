82d5a457bb8a5aa7a8e82bb0ef540b5f652d2c5d
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-11-02 14:35:31 +0700

    mod

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
 
     implementation("org.openapitools:jackson-databind-nullable:0.2.7")
 
