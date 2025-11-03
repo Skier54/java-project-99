e5f65045c0c1fbb3bc7ca18a917241ec69e3366b
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-11-04 01:17:24 +0700

    mod

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,15 +1,16 @@
 package hexlet.code.component;
 
 //import hexlet.code.dto.dtoUser.UserCreateDTO;
-import hexlet.code.mapper.UserMapper;
+//import hexlet.code.mapper.UserMapper;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import hexlet.code.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 //import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,21 +18,24 @@ import org.springframework.stereotype.Component;
 //@Profile("!test")
 public class DataInitializer implements ApplicationRunner {
 
-    @Autowired
     private final UserRepository userRepository;
 
-    @Autowired
     private final CustomUserDetailsService userService;
 
-    @Autowired
-    private final UserMapper userMapper;
+    //private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         var email = "hexlet@example.com";
-        var userData = new User();
-        userData.setEmail(email);
-        userData.setPasswordDigest("qwerty");
-        userService.createUser(userData);
+        if (userRepository.findByEmail(email).isEmpty()) {
+            var userData = new User();
+            userData.setEmail(email);
+            userData.setPasswordDigest("qwerty");
+
+            userService.createUser(userData);
+            System.out.println("Администратор создан: " + email);
+        } else {
+            System.out.println("Администратор уже существует: " + email);
+        }
     }
 }
